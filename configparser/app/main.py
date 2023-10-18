4314d6d9a8a93168ce8754ae744e73f763b34c2e
Barry Weiss <barweiss@cisco.com>
2023-10-18 12:33:01 -0400

    Update documentation for configparser

--- configparser/app/main.py
+++ configparser/app/main.py
@@ -2,24 +2,36 @@ import os
 
 from flask import Flask, render_template
 
+# Initialize the Flask web application
 app = Flask(__name__)
 
-port = os.environ['port']
-environment = os.environ['environment']
-hostname = os.environ['hostname']
-theme_type = os.environ['theme_type']
-debug = os.environ['debug_on']
+# Retrieve environment variables to configure the application
+port = os.environ['port']                # Port on which the application will run
+environment = os.environ['environment']  # Application environment (e.g., 'prod' or 'dev')
+hostname = os.environ['hostname']        # Hostname for the application
+theme_type = os.environ['theme_type']    # UI Theme type (e.g., 'light' or 'dark')
+debug = os.environ['debug_on']           # Debug mode for the application (True or False)
 
 
 @app.route('/', methods=['GET'])
 def index():
+    """
+    Handles the root endpoint of the application and returns the rendered index.html template.
+
+    The template uses variables like hostname, theme_type, and environment to customize its content.
+
+    Returns:
+        render_template: A rendered HTML template.
+    """
     return render_template(
         'index.html',
         hostname=hostname,
         theme_type=theme_type,
         environment=environment,
-        )
+    )
 
 
 if __name__ == "__main__":
+    # This block ensures the Flask app runs only if this script is executed directly
+    # and not imported as a module elsewhere.
     app.run(host='0.0.0.0', port=port, debug=debug)
--- configparser/script.py
+++ configparser/script.py
@@ -1,68 +1,86 @@
-#! /usr/bin/env python3
-
 import configparser
 from textwrap import dedent
 
 from jinja2 import Template
 
-# Set config parser instance
+__author__ = "Barry Weiss - barweiss@cisco.com"
+
+"""
+This script generates a Docker Compose file for a Python web application.
+It reads the configuration from a .ini file, then uses the Jinja2 template
+library to render the Docker Compose configuration with the settings from
+the .ini file. This script is meant for educational purposes.
+"""
+
+# Set up an instance of ConfigParser to read .ini files
 config = configparser.ConfigParser()
 
-# Load the Config .ini file
+# Load the configuration from the 'config.ini' file
 config.read('config.ini')
 
-# Set Prod or Dev Environment
+# Retrieve the desired environment (prod or dev) from the configuration,
+# with 'dev' as the default if not specified
 environment = config.get('DEFAULT', 'environment', fallback='dev')
 
-# Save key values into script
+# Fetch the settings for the desired environment from the .ini file
 py_version = config.get(f'web.app.{environment}', 'py_version')
 version = config.get(f'web.app.{environment}', 'version')
 ip = config.get(f'web.app.{environment}', 'ip')
-port = config.getint(f'web.app.{environment}', 'port')
+port = config.getint(f'web.app.{environment}', 'port')  # get as integer
 hostname = config.get(f'web.app.{environment}', 'hostname')
 theme_type = config.get('web.app.theme', 'type', fallback='light')
-debug_on = config.getboolean('web.app.logging', 'debug_on')
+debug_on = config.getboolean('web.app.logging', 'debug_on')  # get as boolean
 
-
+# Define the Docker Compose template with placeholders for the Jinja2 library
 dockercompose_template = dedent("""\
+    # Docker Compose version
     version: "3"
 
+    # Define the services for our app
     services:
       {{ hostname }}:
         build:
-          context: .
-          dockerfile_inline: |
+          context: .  # Current directory as build context
+          dockerfile_inline: |  # Inline Dockerfile definition
             FROM python:{{ py_version }}-bookworm
 
             LABEL environment={{ environment }}
             LABEL version={{ version }}
 
-            WORKDIR /app
+            WORKDIR /app  # Working directory in the container
 
+            # Copying app dependencies
             COPY ./requirements.txt .
 
+            # Copying the app source code
             COPY ./app/ /app
 
+            # Installing app dependencies
             RUN pip install -U pip && \\
               pip install -r requirements.txt
 
+            # Setting environment variables in the container
             ENV hostname={{ hostname }}
             ENV environment={{ environment }}
             ENV theme_type={{ theme_type }}
             ENV port={{ port }}
             ENV debug_on={{ debug_on }}
 
+            # Expose the desired port for the service
             EXPOSE {{ port }}
 
+            # Command to run on container start
             CMD ["python3", "main.py"]
         container_name: {{ hostname }}
         hostname: {{ hostname }}
         networks:
+          # Use different networks based on environment
           {{ 'dev-appnet' if environment == 'dev' else 'prod-appnet' }}:
             ipv4_address: "{{ ip }}"
         ports:
-          - "{{ port }}:{{ port }}"
+          - "{{ port }}:{{ port }}"  # Map host port to container port
 
+    # Define the networks used in services
     networks:
       dev-appnet:
         ipam:
@@ -74,8 +92,10 @@ dockercompose_template = dedent("""\
             - subnet: 172.20.0.0/24
 """)
 
+# Create a Jinja2 template instance from the Docker Compose template
 jinja_env = Template(dockercompose_template)
 
+# Render the Docker Compose configuration using the values from the .ini file
 dockercompose = jinja_env.render(
     py_version=py_version,
     version=version,
@@ -87,9 +107,11 @@ dockercompose = jinja_env.render(
     port=port,
     )
 
+# Write the rendered Docker Compose configuration to a file
 with open('docker-compose.yml', 'w', encoding='UTF-8') as f:
     f.write(dockercompose)
 
+# Display the rendered Docker Compose configuration for user verification
 print("="*80, "\n DOCKER COMPOSE FILE RENDERED:\n\n")
 print(dockercompose)
 print("="*80, "\nTo run the application enter the following:\n\n > docker compose up")
